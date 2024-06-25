31e0afe85cfd44263f73230fb50e38abd202db75
Nicholas Rose <ntrose90@gmail.com>
2024-06-24 22:45:04 -0700

    seed database

--- server/seed.py
+++ server/seed.py
@@ -13,6 +13,7 @@ with app.app_context():
     pets.append(Pet(name = "Fido", species = "Dog"))
     pets.append(Pet(name = "Whiskers", species = "Cat"))
     pets.append(Pet(name = "Hermie", species = "Hamster"))
+    pets.append(Pet(name = "Slither", species = "Snake"))
 
     # Insert each Pet in the list into the database table
     db.session.add_all(pets)
